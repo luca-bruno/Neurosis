23e5c26e8d66099aefa8b5622e9b724476f8f8b0
luca-bruno <56382257+luca-bruno@users.noreply.github.com>
2020-02-03 19:58:59 +0100

    Player movement animations added + Jumping mechanic revised

--- Neurosis/Assets/Scripts/PlayerMove.cs
+++ Neurosis/Assets/Scripts/PlayerMove.cs
@@ -5,39 +5,61 @@ public class PlayerMove : MonoBehaviour
 {
 	private Rigidbody2D rb;
 	private SpriteRenderer spr;
-	private float moveInput;
-	public float speed = 5f;
-	public float jumpForce = 5f;
-	private bool facingRight = true;  // For determining which way the player is currently facing.
-		
+	private Animator anim;
+	public float speed;
+	public float jumpForce;
+	private bool facingRight;  // For determining which way the player is currently facing.
+	private bool isGrounded;
+	public Transform groundCheck;
+	public LayerMask whatisGround;
 
     void Start(){
 		rb = GetComponent<Rigidbody2D>();
 		spr = GetComponent<SpriteRenderer>();
+		anim = GetComponent<Animator>();
 	}
 
 	void Update(){
-		moveInput = Input.GetAxis("Horizontal");
-		rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+		//moveInput = Input.GetAxis("Horizontal"); //takes x axis position value into float
+	//	rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-		if(moveInput < 0){
+		if(Physics2D.Linecast(transform.position, groundCheck.position, whatisGround)){
+			isGrounded = true;
+		}
+		else {
+			isGrounded = false;
+		}
+
+		if(Input.GetKey("left") || (Input.GetKey("a"))){ //if moves left
+			rb.velocity = new Vector2(speed * -1, rb.velocity.y);
+		
+			if (isGrounded == true){
+				anim.Play("Player_Run");
+			}
+		
 			facingRight = false;
 			spr.flipX = true;
-		} else if (moveInput > 0){
+		} else if (Input.GetKey("right") || (Input.GetKey("d"))){ //if moves right
+			rb.velocity = new Vector2(speed, rb.velocity.y);
+		
+			if (isGrounded == true){
+			anim.Play("Player_Run");
+			}
+		
 			facingRight = true;
 			spr.flipX = false;
 		}
+		else {
+			if (isGrounded == true){
+			anim.Play("Player_Idle");
+			}
+			
+			rb.velocity = new Vector2(0, rb.velocity.y);
+		}
+		
+		if ((Input.GetKey("space") || Input.GetKey("w")) && isGrounded == true){
+			anim.Play("Player_Jump");
+			rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+		}
 	}
-
-
-	// private void Flip()
-	// {
-	// 	// Switch the way the player is labelled as facing.
-	// 	facingRight = !facingRight;
-
-	// 	// Multiply the player's x scale by -1.
-	// 	Vector3 Scaler = transform.localScale;
-	// 	Scaler.x *= -1;
-	// 	transform.localScale = Scaler;
-	}
-//}
+}
