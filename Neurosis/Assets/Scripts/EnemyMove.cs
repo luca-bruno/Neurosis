d27105d5b35c7d47a3e28846c284757b103f4d14
luca-bruno <56382257+luca-bruno@users.noreply.github.com>
2020-02-04 08:18:47 +0100

    Enemy spawning + behaviours

---
+++ Neurosis/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    
+	Rigidbody2D rb;
+	GameObject target;
+	float moveSpeed;
+	Vector3 directionToTarget;
+	public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find ("Player");
+		rb = GetComponent<Rigidbody2D> ();
+		moveSpeed = Random.Range (1f, 3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveEnemy ();
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		switch (col.gameObject.tag) {
+
+		case "Player":
+			EnemySpawn.spawnAllowed = false;
+			Instantiate (explosion, col.gameObject.transform.position, Quaternion.identity);
+			Destroy (col.gameObject);
+			target = null;
+			break;
+
+		}
+	}
+
+	void MoveEnemy ()
+	{
+		if (target != null) {
+			directionToTarget = (target.transform.position - transform.position).normalized;
+			rb.velocity = new Vector2 (directionToTarget.x * moveSpeed,
+										directionToTarget.y * moveSpeed);
+		}
+		else
+			rb.velocity = Vector3.zero;
+	}
+}
+
---
+++ Neurosis/Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] enemies;
+    int randomSpawnPoint, randomEnemy;
+    public static bool spawnAllowed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     spawnAllowed = true;
+     InvokeRepeating ("SpawnAnEnemy", 0f, 1f);   
+    }
+
+    // Update is called once per frame
+    void SpawnAnEnemy()
+    {
+        if (spawnAllowed == true){
+            randomSpawnPoint = Random.Range (0, spawnPoints.Length);
+            randomEnemy = Random.Range (0, enemies.Length);
+            Instantiate (enemies [randomEnemy], spawnPoints [randomSpawnPoint].position, Quaternion.identity);
+        }
+    }
+}
